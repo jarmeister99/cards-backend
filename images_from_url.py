96ad469768a3b39dc391c15e82307f8addbe7166
Jared Foster <jfoster@rantec.com>
2021-11-12 12:24:38 -0800

    created get_image_or_screenshot() function

--- images_from_url.py
+++ images_from_url.py
@@ -1,15 +1,34 @@
+import time
+from typing import Literal, Union, NamedTuple
+
 import requests
 import bs4
 import math
+import re
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+def get_screenshot(url: str):
+    chrome_options = Options()  #
+    chrome_service = Service("./chromedriver.exe")
+    chrome_options.headless = True
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    browser = webdriver.Chrome(options=chrome_options, service=chrome_service)
+    browser.get(url)
+    time.sleep(1)
+    return browser.get_screenshot_as_base64()
 
 
 def get_images(
-    url: str,
-    desired_aspect_ratio: float,
-    min_width: float = 0,
-    max_width: float = math.inf,
-    min_height: float = 0,
-    max_height: float = math.inf,
+        url: str,
+        desired_aspect_ratio: float = 16 / 9,
+        min_width: float = 0,
+        max_width: float = math.inf,
+        min_height: float = 0,
+        max_height: float = math.inf,
 ):
     # get and parse html content
     r = requests.get(url)
@@ -21,19 +40,19 @@ def get_images(
         if tag.get("src"):
             # Ensure height and width are given
             if (height := tag.attrs.get("height")) and (
-                width := tag.attrs.get("width")
+                    width := tag.attrs.get("width")
             ):
                 # Ensure height and width are in specified range
                 if (
-                    min_height <= float(height) <= max_height
-                    and min_width <= float(width) <= max_width
+                        min_height <= float(height) <= max_height
+                        and min_width <= float(width) <= max_width
                 ):
                     # Check to see if this aspect ratio is the closest match seen thus far
                     ratio = float(width) / float(height)
                     if closest_ratio:
                         # Is the current aspect ratio closer to the target than the previous best match?
                         if abs(closest_ratio - desired_aspect_ratio) > abs(
-                            ratio - desired_aspect_ratio
+                                ratio - desired_aspect_ratio
                         ):
                             # mark as new best match
                             closest_tag, closest_ratio = tag, ratio
@@ -42,21 +61,59 @@ def get_images(
                         closest_tag, closest_ratio = tag, ratio
 
     if closest_tag:
-        return closest_tag.get("src")
+        stripped_src = closest_tag.get("src").lstrip("/")
+        if re.match(
+                r"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)",
+                stripped_src,
+        ):
+            return stripped_src
+        else:
+            return f"{url}{stripped_src}"
     else:
         return None
 
 
+ImageResult = NamedTuple('ImageResult', type=str, img_url=str)
+
+
+def get_image_or_screenshot(
+        url: str,
+        desired_aspect_ratio: float = 16 / 9,
+        min_width: float = None,
+        max_width: float = None,
+        min_height: float = None,
+        max_height: float = None,
+) -> (Union[Literal["BASE64"], Literal["LINK"]], str):
+    img_url = get_images(url=url,
+                         desired_aspect_ratio=desired_aspect_ratio,
+                         min_width=min_width,
+                         max_width=max_width,
+                         min_height=min_height,
+                         max_height=max_height)
+    if not img_url:
+        return_type = 'BASE64'
+        img_url = get_screenshot(url=url)
+    else:
+        return_type = 'LINK'
+    return ImageResult(return_type, img_url)
+
+
 if __name__ == "__main__":
-    foo = get_images(
-        "https://www.loveandlemons.com/baked-potato/",
-        desired_aspect_ratio=16 / 9,
-        min_width=100,
+    # foo = get_images(
+    #     "https://www.loveandlemons.com/baked-potato/",
+    #     desired_aspect_ratio=16 / 9,
+    #     min_width=100,
+    # )
+    # bar = get_images(
+    #     "https://en.wikipedia.org/wiki/Foobar",
+    #     desired_aspect_ratio=16 / 9,
+    #     min_width=100,
+    # )
+    # baz = get_images(
+    #     "https://google.com",
+    #     desired_aspect_ratio=16 / 9
+    # )
+    img_result = get_image_or_screenshot(
+        "https://adamschwartz.co/magic-of-css/"
     )
-    bar = get_images(
-        "https://en.wikipedia.org/wiki/Foobar",
-        desired_aspect_ratio=16 / 9,
-        min_width=100,
-    )
-    print(foo)
-    print(bar)
+    print(img_result)
